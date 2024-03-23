cbcf0f4c4875b86a2cb899bef92725268f9b95f9
DRobinson <drobinson@benchmarkeducation.com>
2024-03-23 19:16:52 -0400

    make responsive

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './assets/AppRouter'
+import AppRoutes from './AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
