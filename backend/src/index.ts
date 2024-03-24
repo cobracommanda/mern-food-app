043f78f3b9c34edff3865f8dba65b49a3d2dc2e6
DRobinson <drobinson@benchmarkeducation.com>
2024-03-24 13:24:17 -0400

    add backend folder to previous commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,17 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(()=>console.log("Connected to database!"))
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello!" });
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(3000, () => {
   console.log("server started on localhost:3000");
