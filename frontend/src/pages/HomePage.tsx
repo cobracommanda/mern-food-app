b7b2e52f849219bc08beef230638076e73974999
DRobinson <drobinson@benchmarkeducation.com>
2024-04-06 15:21:44 -0400

    searchbar component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,20 +1,33 @@
 import landingImage from "../../src/assets/landing.png";
 import appDownloadImage from "../../src/assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-500">
-          Tuck into a takeout today
+          Savor a carryout meal today
         </h1>
-        <span className="text-xl">Food is just a click away</span>
+        <span className="text-xl">Eats are just a tap apart</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
-            Order takeout even faster
+            Get meals quicker online
           </span>
           <span>
             Download the NardtEats App for faster ordering and personalized
